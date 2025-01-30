53eb1d3756ac9477fe069e969d0faa55bb6a41cf
John_Dre <jonathan.dresemann@icloud.com>
2025-01-30 13:53:00 +0100

    Player Rotation

--- main.py
+++ main.py
@@ -17,10 +17,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.update(dt)
         player.draw(screen)
+        
         pygame.display.flip()
         clock.tick(60)
-        dt = clock.tick()/1000
+        dt = clock.tick(60)/1000
+        
     #print("Starting asteroids!")
     #print(f"Screen width: {SCREEN_WIDTH}")
     #print(f"Screen height: {SCREEN_HEIGHT}")
